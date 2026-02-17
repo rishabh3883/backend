7cb52e922821745e37c49a537173c94fd44c0948
Rishabh Gupta <146321370+rishabh3883@users.noreply.github.com>
2026-02-17 09:31:53 +0530

    Add '/register' route to authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 const authMiddleware = require('../middlewares/authMiddleware');
-
+router.post('/register', authController.register);
 router.post('/signup', authController.register);
 router.post('/login', authController.login);
 router.get('/me', authMiddleware(['Student', 'Admin', 'Employee']), authController.getMe);
